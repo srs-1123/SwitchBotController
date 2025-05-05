7e547e939c5f409a02b56953a22703d526c6e9e0
srs-1123 <srs.23p2.1123@gmail.com>
2025-05-05 23:52:19 +0900

    add VCNL4040Sensor

--- src/infrastructure/sensors/VCNL4040Sensor.hpp
+++ src/infrastructure/sensors/VCNL4040Sensor.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <memory>
 #include "brightness.hpp"
 #include "brightnessSensor.hpp"
 
@@ -13,13 +14,24 @@ using domain::model::BrightnessSensor;
 class VCNL4040Sensor: public BrightnessSensor {
 public:
     VCNL4040Sensor(const char *i2c_device);
-    ~VCNL4040Sensor();
+    ~VCNL4040Sensor() = default;
 
     Brightness getBrightness() override;
+    uint16_t SetALSConfig(uint8_t cmd);
 
 private:
+#if 0
     int fd;
-    uint16_t SetALSConfig(uint8_t cmd);
+#else
+    class FileDescriptorDeleter;
+    std::unique_ptr<int, FileDescriptorDeleter> fd;
+#endif
+
+    static constexpr uint8_t ALS_CONF = 0x00;
+    static constexpr uint8_t I2C_ADDRESS = 0x60;
+    static constexpr uint8_t DEVICE_ID = 0x0C;
+    static constexpr uint8_t ALS_DATA = 0x09;
+
     uint16_t readRegister(uint8_t reg_addr);
     uint16_t writeRegister(uint8_t reg_addr, uint8_t lsb, uint8_t msb);
 };
